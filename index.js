97f17bd813f779b6cfe8c6df2adfb4a1b17dcfc6
dhr231 <dhruvprashar231@gmail.com>
2021-09-20 16:42:39 +0530

    some progress

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ app.use(express.json())
 
 app.get("/api/search",(req,res)=>{
     let keyword = req.query['q'];
-    request('https://coolrom.com.au/search?q='+keyword).on('data',(data)=>{
-        const $ = Cheerio.load(data);
-        $('a').each((i)=>{
-            dataarr.push($(this).text())
-        })
-        res.send(dataarr)
+    dataarr = []
+    let page_number = req.query['page']
+    request(`https://www.gamulator.com/search.php?currentpage=${page_number}&search_term_string=${keyword}`).on('data',(data)=>{
+       const $ = Cheerio.load(data);
+       $('img.img-fluid').each(function(i){
+           dataarr.push({
+              img: 'https://www.gamulator.com'+$(this).attr('src')  ,
+              title:$('h5.card-title').eq(i).text(),
+              url:$('div.card > a').eq(i).attr('href'),
+            })
+       })
+       res.send(dataarr);
     })
 })
 
